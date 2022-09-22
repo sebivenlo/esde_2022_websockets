7ba3b12e594a181f66fed83d15cc1188d86a0721
ayman184 <54893167+ayman184@users.noreply.github.com>
2022-09-22 10:32:35 +0200

    fixed css file

--- index.js
+++ index.js
@@ -8,16 +8,28 @@ var messages = [];
 
 console.clear();
 
-io.on("connection", () => {
-  /* … */
+io.on("connection", (socket) => {
+  console.log(`A new client connected!`.brightGreen);
+
+  socket.emit("chat:messages", messages);
+
   io.on("chat:message", (payload) => {
-    console.log(`Received event chat:message with payload ${payload}`);
+    console.log(`Received event chat:message with payload ${payload}`.yellow);
+    if (payload.message && typeof payload.message === "string") {
+      //emits the events to all connected clients except for the sender
+      socket.broadcast.emit(payload.message);
+
+      //push to global array
+      messages.push(payload.message);
+    }
   });
 });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.use(express.static("public"));
 
 const port = process.env.PORT || 3000;
 
