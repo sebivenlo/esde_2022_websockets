db545fb8202545f431552346a0d355bb328cd38d
Mo <mo.elgendy1996@gmail.com>
2022-11-13 16:25:07 +0100

    chat implementation

--- index.js
+++ index.js
@@ -1,38 +1,75 @@
-const express = require("express");
-const colors = require("colors");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./helpers/formatDate')
+const {
+  getActiveUser,
+  exitRoom,
+  newUser,
+  getIndividualRoomUsers
+} = require('./helpers/userHelper');
+
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const server = http.createServer(app);
+const io = socketio(server);
 
-var messages = [];
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-console.clear();
+// this block will run when the client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = newUser(socket.id, username, room);
 
-io.on("connection", (socket) => {
-  console.log(`A new client connected!`.brightGreen);
+    socket.join(user.room);
 
-  socket.emit("chat:messages", messages);
+    // General welcome
+    socket.emit('message', formatMessage("WebCage", 'Messages are limited to this room! '));
 
-  io.on("chat:message", (payload) => {
-    console.log(`Received event chat:message with payload ${payload}`.yellow);
-    if (payload.message && typeof payload.message === "string") {
-      //emits the events to all connected clients except for the sender
-      socket.broadcast.emit(payload.message);
+    // Broadcast everytime users connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage("WebCage", `${user.username} has joined the room`)
+      );
 
-      //push to global array
-      messages.push(payload.message);
+    // Current active users and room name
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getIndividualRoomUsers(user.room)
+    });
+  });
+
+  // Listen for client message
+  socket.on('chatMessage', msg => {
+    const user = getActiveUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = exitRoom(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage("WebCage", `${user.username} has left the room`)
+      );
+
+      // Current active users and room name
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getIndividualRoomUsers(user.room)
+      });
     }
   });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
+const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
+app.use(express.static("helpers"));
 
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Listening on port ${port}`.brightGreen);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
